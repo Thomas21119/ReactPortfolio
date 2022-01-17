3d034f3b4e4a6cf73e9a18e93f7509b8f1f311a6
Thomas21119 <thomas.wittwer2018@gmail.com>
2022-01-17 12:17:43 +1100

    toggleHover func works

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,35 +1,100 @@
-import React from 'react';
+import { func } from 'prop-types';
+import React, { useState } from 'react';
 import './Portfolio.css';
 
 function Portfolio() {
+  function toggleHover(e) {
+    const test = document.querySelector('#psuedoShopper');
+    if (test.className === 'description') {
+      console.log('test');
+      test.classList.add('hidden');
+    } else {
+      console.log('fail');
+      test.classList.remove('hidden');
+    }
+  }
+
   return (
     <div className="portfolio">
       <h3 className="sectionHeading"> My Projects </h3>
       <div>
-        <img
-          src="https://github.com/Thomas21119/ReactPortfolio/blob/main/public/images/Flight%20Finder.png?raw=true"
-          alt="flightFinder"
-        />
-        <img
-          src="https://github.com/Thomas21119/ReactPortfolio/blob/main/public/images/PsuedoShopper.png?raw=true"
-          alt="PsuedoShopper"
-        />
-        <img
-          src="https://github.com/Thomas21119/ReactPortfolio/blob/main/public/images/PsuedoShopper.png?raw=true"
-          alt="PsuedoShopper"
-        />
-        <img
-          src="https://github.com/Thomas21119/ReactPortfolio/blob/main/public/images/PsuedoShopper.png?raw=true"
-          alt="PsuedoShopper"
-        />
-        <img
-          src="https://github.com/Thomas21119/ReactPortfolio/blob/main/public/images/PsuedoShopper.png?raw=true"
-          alt="PsuedoShopper"
-        />
-        <img
-          src="https://github.com/Thomas21119/ReactPortfolio/blob/main/public/images/PsuedoShopper.png?raw=true"
-          alt="PsuedoShopper"
-        />
+        {/* want to pass a function parameter through this so im not retyping code */}
+        <div className="projects" onClick={toggleHover}>
+          <h3> PsuedoShopper </h3>
+          <img
+            src="https://github.com/Thomas21119/ReactPortfolio/blob/main/public/images/PsuedoShopper.png?raw=true"
+            alt="PsuedoShopper"
+          />
+          <p className="description" id="psuedoShopper">
+            PsuedoShopper is a similar clone to an Ebay in which users can
+            create their own accounts and buy or sell items that they have put
+            up for auction. This is the project i worked on most recently with
+            Tom H, Mac and Ruchi.
+          </p>
+        </div>
+        <div className="projects">
+          <h3> Flight Finder</h3>
+          <img
+            src="https://github.com/Thomas21119/ReactPortfolio/blob/main/public/images/Flight%20Finder.png?raw=true"
+            alt="flightFinder"
+          />
+          <p className="description">
+            Flight Finder is the other Project i've worked on in a group with
+            Ignacio, Trang and Ruchi. This site uses various Api's to find
+            information on flights and present it to the users, as well as
+            finding locations of interest around the destination
+          </p>
+        </div>
+        <div className="projects" onMouseOver={toggleHover}>
+          <h3> Work Day Scheduler </h3>
+          <img
+            src="https://github.com/Thomas21119/ReactPortfolio/blob/main/public/images/Work%20Day%20Scheduler.png?raw=true"
+            alt="Work Day Scheduler"
+          />
+          <p className="description">
+            The Work Day Scheduler is a simple yet effective color coded
+            Scheduler from the hours of 9-5 So that people can plan out their
+            day with less distractions
+          </p>
+        </div>
+        <div className="projects" onMouseOver={toggleHover}>
+          <h3> Note Taker </h3>
+          <img
+            src="https://github.com/Thomas21119/ReactPortfolio/blob/main/public/images/Note%20Taker%20.png?raw=true"
+            alt="Note Taker"
+          />
+          <p className="description">
+            Note Taker is a place to Take notes, which are namable and gets
+            saved to your local device so that upon page refresh the same
+            information is still there
+          </p>
+        </div>
+        <div className="projects" onMouseOver={toggleHover}>
+          <h3> Password Generator </h3>
+          <img
+            src="https://github.com/Thomas21119/ReactPortfolio/blob/main/public/images/Password%20Generator.png?raw=true"
+            alt="Password Generator"
+          />
+          <p className="description">
+            The password generator though basic was my first attempt at
+            javascript, and though it lacks security and a way to safely save
+            the information, i am very proud of what i accomplished given my
+            lack of experience
+          </p>
+        </div>
+        <div className="projects" onMouseOver={toggleHover}>
+          <h3> TechBlog </h3>
+          <img
+            src="https://github.com/Thomas21119/ReactPortfolio/blob/main/public/images/dashboard.png?raw=true"
+            alt="TechBlog"
+          />
+          <p className="description">
+            The Tech blog is of current, my only solo website that is fully
+            developed and allows communication between parties, requiring a
+            simple login and logout, people can post questions, mainly about
+            software development and have them be responded to by other users
+          </p>
+        </div>
       </div>
     </div>
   );
