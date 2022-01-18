bf545bb7f71bfb7647e9c4276fdbd8e001b8a468
Thomas21119 <thomas.wittwer2018@gmail.com>
2022-01-18 20:38:10 +1100

    Updates

---
+++ precache-manifest.6fa9cf7dedeaef93ffdac9c02e013b31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5774278c7481168eaf62a0c2456789b8",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "1ec6a0cec5dcd81cdbdb",
+    "url": "/ReactPortfolio/static/css/main.dadbc7c3.chunk.css"
+  },
+  {
+    "revision": "78402ece03a0447bba82",
+    "url": "/ReactPortfolio/static/js/2.93bb0c75.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ReactPortfolio/static/js/2.93bb0c75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ec6a0cec5dcd81cdbdb",
+    "url": "/ReactPortfolio/static/js/main.d3e0751c.chunk.js"
+  },
+  {
+    "revision": "97a1c7d0b592075495b7",
+    "url": "/ReactPortfolio/static/js/runtime-main.ad87311a.js"
+  }
+]);
